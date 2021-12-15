5177a97647c3beff30f4bdb191082f1234d5ba41
Tyler Solley <tjsolley1@gmail.com>
2021-12-15 14:11:08 -0800

    finished exam

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -11,7 +11,10 @@ import { Http } from '@angular/http';
 
 export class OrdersComponent implements OnInit {
 
-  orders:Array<any> = [];
+  orders: Array<any> = [];
+  name = '';
+  errorMessage = '';
+  confirmMessage = '';
 
   constructor(
     private router: Router,
@@ -20,23 +23,120 @@ export class OrdersComponent implements OnInit {
   ) {
 
   }
+  submit() {
 
-  async ngOnInit() {
-    
+    const commaIndex = this.name.indexOf(', ');
+    let error = false;
+
+
+
+
+    if (commaIndex === -1) {
+      console.log('Name Must Have a Comma!')
+      this.errorMessage = 'Name Must Have a Comma!';
+      error = true;
+    }
+    if (this.name === '') {
+      console.log('Name Must Not Be Empty!')
+      this.errorMessage = 'Name Must Not Be Empty!';
+      error = true;
+    }
+    else if (commaIndex === -1) {
+      this.errorMessage = 'Name Must Have a Comma!';
+      error = true;
+    }
+    if (!error) {
+      const firstName = this.name.slice(commaIndex + 1, this.name.length);
+      const lastName = this.name.slice(0, commaIndex);
+      const FullName = firstName + ' ' + lastName
+
+      const calculation = this.calculate();
+      this.confirmMessage = 'Thank You For Your Order!'
+      this.flexModal.openDialog('confirm-modal')
+    }
+    else {
+      this.flexModal.openDialog('error-modal');
+    }
   }
 
-  // prepare result, splice last name, first name
+  async ngOnInit() {
+    this.LoadDefaultOrders();
+  }
 
-  // Calculate total and perform input validation
-  
-  // display the order form with orders from orders.json
+  calculate() {
+    const total = this.orders.reduce((inc, item, i, array) => {
+      inc += item.price * item.quantity;
+      return inc;
+    }, 0);
+    const taxAmount = total * .1;
 
-  // Clear the orders form
+    const subTotal = total - taxAmount
+    console.log('from calculate() total: ', total, 'taxAmount', taxAmount, 'subTotal', subTotal);
+    return {
+      total: total
+        taxAmount: taxAmount
+        subTotal: subTotal
+    }
 
-  // Add items 'Hot Dog', 'Hamberger' and 'Pizza' to list when corresponding button is clicked
+  }
+  LoadDefaultOrders() {
+    this.orders = [{
+      "pid": "1",
+      "image": "assets/sm_hotdog.jpeg",
+      "description": "Hot Dog",
+      "price": 5.00,
+      "quantity": 2
+    }, {
+      "pid": "2",
+      "image": "assets/sm_hamberger.jpeg",
+      "description": "Hamberger",
+      "price": 6.00,
+      "quantity": 1
+    }, {
+      "pid": "3",
+      "image": "assets/sm_pizza.jpeg",
+      "description": "Large Pizza",
+      "price": 12.00,
+      "quantity": 2
+    }]
+  }
 
-  // delete line item (order) when delete button is click
+  delete(index: number) {
+    this.orders.splice(index, 1);
+  }
 
-  // read in the orders.json file and populate the list table with the initial orders (3)
+  addItem(item: string) {
+    switch (item) {
+      case 'hot dog':
+        this.orders.unshift({
+          "pid": "1",
+          "image": "assets/sm_hotdog.jpeg",
+          "description": "Hot Dog",
+          "price": 5.00,
+          "quantity": 0
+        })
 
+        break;
+      case 'hamberger':
+        this.orders.unshift({
+          "pid": "2",
+          "image": "assets/sm_hamberger.jpeg",
+          "description": "Hamberger",
+          "price": 6.00,
+          "quantity": 0
+        })
+        break;
+      case 'pizza':
+        this.orders.unshift({
+          "pid": "3",
+          "image": "assets/sm_pizza.jpeg",
+          "description": "Large Pizza",
+          "price": 12.00,
+          "quantity": 0
+        })
+
+        break;
+
+    }
+  }
 }
